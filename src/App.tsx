ccc49e7ab81e89e88333c16a61a1f754a11ec6c5
irlyfizaharis <irlyfizaharis@gmail.com>
2025-03-12 21:37:40 +0700

    Finish UI Authentication: Login Page and Register Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/authentication/login";
+import RegisterPage from "./pages/authentication/register";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"></Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
