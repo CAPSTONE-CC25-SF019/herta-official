a984f40534638e2588466687d9f970d191e0d941
dwincahya <dwincahya8@gmail.com>
2025-03-20 19:57:13 +0700

    CustomIcon and Responsive

--- src/components/general/landing/how-it-works/card-step.tsx
+++ src/components/general/landing/how-it-works/card-step.tsx
@@ -12,12 +12,16 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ card }) => {
   return (
-    <div className="h-full w-full rounded-lg bg-white p-6 shadow-md">
+    <div className="h-full min-h-72 w-full max-w-56 bg-white p-4 shadow-[6px_6px_10px_rgba(0,0,0,0.2)] md:min-h-60 md:max-w-48 lg:min-h-80 lg:max-w-64">
       <div className="space-y-3">
         <div className="text-black">{card.icon}</div>
-        <span className="text-herta-gray font-bold text-sm">{card.title}</span>
-        <h1 className="text-2xl font-bold">{card.subtitle}</h1>
-        <p className="text-md text-black">{card.description}</p>
+        <span className="text-herta-gray text-sm font-bold">{card.title}</span>
+        <h1 className="text-lg font-bold md:text-xl lg:text-2xl">
+          {card.subtitle}
+        </h1>
+        <p className="text-xs text-black md:text-sm lg:text-base">
+          {card.description}
+        </p>
       </div>
     </div>
   );
--- src/pages/Landing/how-it-works.tsx
+++ src/pages/Landing/how-it-works.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import Card from "../../components/general/landing/how-it-works/card-step";
-import {
-  ArrowLeftEndOnRectangleIcon,
-  UserPlusIcon,
-  ChartBarSquareIcon,
-} from "@heroicons/react/24/outline";
+import { UserPlusIcon, ChartBarIcon } from "@heroicons/react/24/outline";
 
 interface Step {
   id: number;
@@ -14,12 +10,31 @@ interface Step {
   icon: React.ReactNode;
 }
 
+const LoginIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 29 24"
+      strokeWidth={0.8}
+      stroke="currentColor"
+      className="h-12"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M20 9V5.25A5 5 0 0 0 17 2.5h-10A3 3 0 0 0 4 5.5v13A3 3 0 0 0 7 21.5h10a3 3 0 0 0 3-3V15M14 8l-6 4m0 0 6 4m-6-4h19"
+      />
+    </svg>
+  );
+};
+
 const HowItWorks: React.FC = () => {
   const steps: Step[] = [
     {
       id: 1,
       title: "Step 1",
-      subtitle: "Register",
+      subtitle: "Register Your Account",
       description:
         "Create your account to access healthcare analytics and predictions.",
       icon: <UserPlusIcon className="h-12" strokeWidth={0.8} />,
@@ -27,30 +42,30 @@ const HowItWorks: React.FC = () => {
     {
       id: 2,
       title: "Step 2",
-      subtitle: "Login",
+      subtitle: "Login to Your Account",
       description:
         "Securely log in to your account to start exploring health data.",
-      icon: <ArrowLeftEndOnRectangleIcon className="h-12" strokeWidth={0.8} />,
+      icon: <LoginIcon />,
     },
     {
       id: 3,
       title: "Step 3",
-      subtitle: "Dashboard",
+      subtitle: "Access Your Dashboard",
       description:
         "Access disease insights, AI health predictions, and analytics tools.",
-      icon: <ChartBarSquareIcon className="h-12" strokeWidth={0.8} />,
+      icon: <ChartBarIcon className="h-12" strokeWidth={0.8} />,
     },
   ];
 
   return (
-    <div className="bg-herta-bg flex min-h-screen flex-col items-center justify-center p-7 md:p-8">
+    <div className="bg-herta-bg flex min-h-screen flex-col items-center justify-center p-4 md:p-8">
       <div className="w-full max-w-6xl">
         <div className="relative mb-8 md:mb-12">
-          <div className="relative mb-8 flex justify-center md:px-12">
-            <div className="flex w-full max-w-md items-center justify-between md:max-w-xl lg:max-w-[47rem] xl:max-w-[52rem]">
+          <div className="relative mb-8 hidden md:flex md:justify-center md:px-12">
+            <div className="flex w-full max-w-md items-center justify-between px-8 md:max-w-[45rem]">
               {steps.map((step, index) => (
                 <React.Fragment key={step.id}>
-                  <div className="border-herta-400 text-herta-500 z-10 flex h-10 w-10 items-center justify-center rounded-full border-3 text-base font-bold md:h-12 md:w-12 md:text-lg">
+                  <div className="border-herta-400 text-herta-500 z-10 flex h-12 w-12 items-center justify-center rounded-full border-3 text-lg font-bold">
                     {step.id < 10 ? `0${step.id}` : step.id}
                   </div>
                   {index < steps.length - 1 && (
@@ -60,7 +75,34 @@ const HowItWorks: React.FC = () => {
               ))}
             </div>
           </div>
-          <div className="grid grid-cols-1 place-items-center gap-4 md:grid-cols-3 md:gap-8">
+
+          <div className="relative md:hidden">
+            <div className="absolute left-5 mt-36 flex flex-col items-center">
+              {steps.map((step, index) => (
+                <React.Fragment key={step.id}>
+                  <div className="border-herta-400 text-herta-500 z-10 flex h-10 w-10 items-center justify-center rounded-full border-3 text-base font-bold">
+                    {step.id < 10 ? `0${step.id}` : step.id}
+                  </div>
+                  {index < steps.length - 1 && (
+                    <div className="bg-herta-400 h-80 w-[3px]"></div>
+                  )}
+                </React.Fragment>
+              ))}
+            </div>
+
+            <div className="ml-24 flex flex-col">
+              {steps.map((step, index) => (
+                <div
+                  key={step.id}
+                  className={`mb-${index < steps.length - 1 ? "24" : "0"} mt-5`}
+                >
+                  <Card card={step} />
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="hidden md:flex md:flex-wrap md:justify-center md:gap-12">
             {steps.map((step) => (
               <div key={step.id} className="flex flex-col">
                 <Card card={step} />
