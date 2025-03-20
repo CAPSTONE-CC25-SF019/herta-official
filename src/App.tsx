422eec81181f1bba8551aae1514d82dccca9e641
irlyfizaharis <irlyfizaharis@gmail.com>
2025-03-20 17:22:19 +0700

    Finish UI History List

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import LoginPage from "./pages/authentication/login";
 import RegisterPage from "./pages/authentication/register";
 import LandingPage from "./pages/landing";
 import DiseasePage from "./pages/disease";
+import HistoryPage from "./pages/history";
 
 function App() {
   // TODO: Implement Layout
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/disease" element={<DiseasePage />} />
+        <Route path="/history" element={<HistoryPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/general/history/card-history.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+interface CardHistoryProps {
+  name: string;
+  tags: string[];
+}
+
+export default function CardHistory({ name, tags }: CardHistoryProps) {
+  return (
+    <Link
+      to={`/`}
+      className="rounded-lg border-[1px] border-[#ffffff] bg-[#fcfcfc] p-2 shadow-sm"
+    >
+      <div className="flex flex-col p-1">
+        <div className="mb-2 h-full grow space-y-2">
+          <div className="flex flex-col">
+            <span className="text-sm text-zinc-600">Your Disease:</span>
+            <h2 className="line-clamp-2 py-1 text-2xl">{name}</h2>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-sm text-zinc-600">Your Indication:</span>
+            <div className="mt-2 flex gap-2">
+              {tags.map((tag) => (
+                <TagHistory name={tag} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+const TagHistory = ({ name }: { name: string }) => (
+  <span className="border-herta-400 text-herta-400 bg-herta-400/20 inline-block w-fit rounded-full border-[1px] px-4 py-[.1rem] text-sm leading-tight">
+    {name}
+  </span>
+);
