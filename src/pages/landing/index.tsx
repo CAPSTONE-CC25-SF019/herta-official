6815971e3eb17903b01a5c33d5a50b9bee10b2ac
Daffabot <daffabot@programmer.net>
2025-03-21 10:59:07 +0700

    Add Navbar and responsive to mobile

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,11 +1,25 @@
 import Hero from "./hero";
 import HomeSection from "./home";
 import Footer from "./footer";
+import Navbar from "../../components/ui/navbar";
 
 export default function LandingPage() {
+  const nav = [{
+    name: "Home",
+    link: "/"
+  },
+  {
+    name: "About",
+    link: "/about"
+  },
+  {
+    name: "Disease",
+    link: "/disease"
+  }];
   return (
     <>
       <Hero />
+      <Navbar srcLogo="../../assets/navbar/logo.png" navItem={nav} />
       <HomeSection />
       <section className="relative bg-herta-200 h-svh"></section>
       <Footer />
